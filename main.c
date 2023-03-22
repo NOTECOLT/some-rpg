7a7b54e3f0c5e40012803b3b464bc75a79d64526
notecolt <d27thColt@gmail.com>
2023-03-22 16:24:42 +0800

    Sorta have grid-based player movement in a semi-working state

--- main.c
+++ main.c
@@ -1,60 +1,87 @@
 #include "raylib.h"
 #include "src/entity.c"
 
-int TILESIZE = 40;
-Vector2 WORLDSIZE = {
-	.x = 20,
-	.y = 20
+int TILE_SIZE = 40;
+Vector2Int WORLD_SIZE = {
+	.x = 39,
+	.y = 22
 };
 
-/** Draws the world **/
-int RenderWorld() {
-	for (int i = 0; i < WORLDSIZE.x; i++) {
-		for (int j = 0; j < WORLDSIZE.y; j++) {
+int P_SPEED_WLK = 350;
+int P_SPEED_RUN = 425;
+
+/*
+IDEAS:
+- Grid Based Movement
+- Render Queue
+- Debug Info Display
+
+*/
+
+/** Draws the world */
+void RenderWorld() {
+	for (int i = 0; i < WORLD_SIZE.x; i++) {
+		for (int j = 0; j < WORLD_SIZE.y; j++) {
 			Color tileColor = ((i + j) % 2) ? LIGHTGRAY : WHITE;
-			DrawRectangle(i * TILESIZE, j * TILESIZE, TILESIZE, TILESIZE, tileColor);
+			DrawRectangle(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE, tileColor);
 		}
 	}
 }
 
-
 int main() {
-	const int screenWidth = 800;
-	const int screenHeight = 450;
+	const int screenWidth = 1600;
+	const int screenHeight = 900;
 
 	InitWindow(screenWidth, screenHeight, "test");
 	SetTargetFPS(60);
+	int frameCounter = 0;
 
 	// PLAYER STUFF
-	Entity player;
-	player.position = (WorldPos){ .x = 0, .y = 0 };
-	player.speed = (Vector2){ .x = 0, .y = 0 };
+	Entity* player = NewEntity((Vector2Int){.x = 0, .y = 0}, TILE_SIZE, P_SPEED_WLK);
 
 	// WORLD STUFF
 
 	while (!WindowShouldClose()) {
+		// 1 - INPUT
+		if (!player->isMoving) {
+			if (IsKeyDown(KEY_RIGHT))
+				player->targetWP = (Vector2Int){.x = player->worldPos.x + 1, .y = player->worldPos.y};
+			else if (IsKeyDown(KEY_LEFT))
+				player->targetWP = (Vector2Int){.x = player->worldPos.x - 1, .y = player->worldPos.y};
+			else if (IsKeyDown(KEY_DOWN))
+				player->targetWP = (Vector2Int){.x = player->worldPos.x, .y = player->worldPos.y + 1};
+			else if (IsKeyDown(KEY_UP))
+				player->targetWP = (Vector2Int){.x = player->worldPos.x, .y = player->worldPos.y - 1};
+			else
+				player->targetWP = (Vector2Int){.x = player->worldPos.x, .y = player->worldPos.y};
+		}
+
+		// 2 - PHYSICS
+
+		UpdateEntityVectors(player, TILE_SIZE);
+
+		// 3 - RENDERING
+
         BeginDrawing();
 		ClearBackground(RAYWHITE);
 
-		// INPUT
-		if (IsKeyDown(KEY_RIGHT))
-			player.position.x += 1;
-		else if (IsKeyDown(KEY_LEFT))
-			player.position.x -= 1;
-
-		if (IsKeyDown(KEY_DOWN))
-			player.position.y += 1;
-		else if (IsKeyDown(KEY_UP))
-			player.position.y -= 1;
-
-
-		//UpdateEntityVectors(&player);
-
 		RenderWorld();
-		RenderEntity(player, TILESIZE);
+		RenderEntity(*player, TILE_SIZE);
+		DrawText(TextFormat("f: %d; fps: %d", frameCounter, GetFPS()), 5, 5, 30, BLACK);
+		DrawText(TextFormat("position: (%f, %f)", player->position.x, player->position.y), 5, 40, 30, BLACK);
+		DrawText(TextFormat("worldPos: (%d, %d)", player->worldPos.x, player->worldPos.y), 5, 75, 30, BLACK);
+		DrawText(TextFormat("targetWP: (%d, %d)", player->targetWP.x, player->targetWP.y), 5, 110, 30, BLACK);
+		float distX = abs(player->position.x - (float)player->targetWP.x * TILE_SIZE);
+		float distY = abs(player->position.y - (float)player->targetWP.y * TILE_SIZE);
+		DrawText(TextFormat("distance: (%f, %f)", distX, distY), 5, 140, 30, BLACK);
+		DrawText(TextFormat("isMoving: %d", player->isMoving), 5, 175, 30, BLACK);
 
-        EndDrawing();		
+        EndDrawing();
+
+		frameCounter++;
 	}
+
+	free(player);
 
 	CloseWindow();
 
--- src/entity.c
+++ src/entity.c
@@ -1,22 +1,86 @@
+#include <stdlib.h>
 #include "raylib.h"
 
-typedef struct worldPos {
+typedef struct vector2Int {
     int x;
     int y;
-} WorldPos;
+} Vector2Int;
 
+/** An Entity is any moving/interactable object in the overworld */
 typedef struct entity {
-	//Vector2 position;
-    WorldPos position;
-	Vector2 speed;
+	Vector2 position;		// Refers to the position of the player with respect to the screen / global coordinate system  
+    Vector2Int worldPos;    // Refers to the position of player relative to the world grid
+
+	int isMoving;
+	Vector2Int targetWP;	// Entity's target world vector. Each entity will constantly move to this location if it is not already
+	float speed;
+	//Vector2 speed;
 } Entity;
 
-/** Updates Player Movement by adding speed to position vectors **/
-void UpdateEntityVectors(Entity* e) {
-	e->position.x += e->speed.x;
-	e->position.y += e->speed.y;
+Entity* NewEntity(Vector2Int worldPos, int tileSize, float speed);
+void UpdateEntityVectors(Entity* e, int tileSize);
+void RenderEntity(Entity e, int tileSize);
+
+
+
+
+/** Creates a new entity in the world */
+Entity* NewEntity(Vector2Int worldPos, int tileSize, float speed) {
+	Entity* e = malloc(sizeof(Entity));
+	e->worldPos = worldPos;
+	e->targetWP = worldPos;
+	e->position = (Vector2){.x = worldPos.x * tileSize, .y = worldPos.y * tileSize};
+	e->isMoving = 0;
+	e->speed = speed;
+
+	return e;
+}
+
+/** Updates Player Movement by adding speed to position vectors */
+void UpdateEntityVectors(Entity* e, int tileSize) {
+	float distX = abs(e->position.x - (float)e->targetWP.x * tileSize);
+	float distY = abs(e->position.y - (float)e->targetWP.y * tileSize);
+
+	// Second Condition is just a bandaid solution, if ever movement glitches & player moves past their target position
+	//		Idea is that a the target position will never be more than 1 tile away, so if ever it does, 
+	//			then something is wrong
+	// @NOTECOLT pls find a better way to do this
+	if (distX < 0.005 && distY < 0.05 || (distX > tileSize + 0.05 || distY > tileSize + 0.05)) {
+		e->worldPos = e->targetWP;
+		e->position = (Vector2){
+			.x = e->worldPos.x * tileSize,
+			.y = e->worldPos.y * tileSize
+		};
+		e->isMoving = 0;
+		return;
+	}
+
+	e->isMoving = 1;
+
+	// If current position of the entity does not match where it needs to be, then move to those coordinates respectively
+	// There is no diagonal movement
+
+	int x = e->targetWP.x - e->worldPos.x;
+	int signX = (x > 0) - (x < 0);
+	int y = e->targetWP.y - e->worldPos.y;
+	int signY = (y > 0) - (y < 0);
+
+	if (e->worldPos.x * signX < e->targetWP.x * signX) {
+		e->position.x += signX * e->speed * GetFrameTime();
+	} else if (e->worldPos.y * signY < e->targetWP.y * signY) {
+		e->position.y += signY * e->speed * GetFrameTime();	
+	}
+	// if (e->worldPos.x != e->targetWP.x) {
+	// 	int x = e->targetWP.x - e->worldPos.x;
+	// 	int sign = (x > 0) - (x < 0);
+	// 	e->position.x += sign * e->speed * GetFrameTime();
+	// } else if (e->worldPos.y != e->targetWP.y) {
+	// 	int y = e->targetWP.y - e->worldPos.y;
+	// 	int sign = (y > 0) - (y < 0);
+	// 	e->position.y += sign * e->speed * GetFrameTime();	
+	// }
 }
 
 void RenderEntity(Entity e, int tileSize) {
-    DrawRectangle(e.position.x * tileSize, e.position.y * tileSize, tileSize, tileSize, RED);
+	DrawRectangle(e.position.x, e.position.y, tileSize, tileSize, RED);
 }
