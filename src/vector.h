d945cecb1f93c4bdce69481739ca507c0d6b6376
notecolt <d27thColt@gmail.com>
2023-03-27 00:16:31 +0800

    Removed this cuz, Vector2Int struct is redundant and unnecessary

--- src/vector.h
+++ src/vector.h
@@ -1,10 +1,10 @@
-#ifndef VECTOR_H
-#define VECTOR_H
+// #ifndef VECTOR_H
+// #define VECTOR_H
 
-/** Vector2 Struct that uses integers instead of vectors */
-typedef struct vector2Int {
-    int x;
-    int y;
-} Vector2Int;
+// /** Vector2 Struct that uses integers instead of vectors */
+// typedef struct vector2Int {
+//     int x;
+//     int y;
+// } Vector2Int;
 
-#endif
+// #endif
