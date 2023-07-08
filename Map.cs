5ff9328b5ca84a751cfdc06c689a66090433df8e
NOTECOLT <6955795+NOTECOLT@users.noreply.github.com>
2023-07-08 12:41:28 +0800

    Added automatic save option

--- Map.cs
+++ Map.cs
@@ -7,6 +7,9 @@ using Newtonsoft.Json;
 using Raylib_cs;
 
 namespace Topdown {
+	/// <summary>
+	/// Map Object. Does not serialize into json object.
+	/// </summary>
     class Map {
 		// STATIC VARIABLES
 		//------------------------------------------------------------------------------------------
@@ -25,6 +28,12 @@ namespace Topdown {
 		public Tilemap Tilemap { get { return _tilemap; } }
 		public Vector2 Size { get { return _size; } }
 
+		/// <summary>
+		/// Creates a new map object
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="path">Specifies the path to the tilemap</param>
+		/// <param name="size"></param>
 		public Map(string name, string path, Vector2 size) {
 			_name = name;
 			_tilemapPath = path;
@@ -56,10 +65,42 @@ namespace Topdown {
 				return null;
 			}
 
-			Map map = new Map(mapJSON.Name, mapJSON.TilemapPath, mapJSON.Size) {
-				_data = mapJSON.Data,
-			};
+			Map map = new Map(mapJSON.Name, mapJSON.TilemapPath, mapJSON.Size);
+
+			// In case that the size of the map in the JSON is not equal to the actual dimensions of the data,
+			//		We just create a map with the specified size, and then copy the data from the JSON to the map obj
+			map._data = new int[(int)mapJSON.Size.X,(int)mapJSON.Size.Y];
+			for (int i = 0; i < map._size.X; i++) {
+				for (int j = 0; j < map._size.Y; j++) {
+					if (i < mapJSON.Data.GetLength(0) && j < mapJSON.Data.GetLength(1))
+						map._data[i,j] = mapJSON.Data[i,j];
+					else
+						map._data[i,j] = -1;
+				}
+			}
+
+			Console.WriteLine($"[MAP LOADER] Map succesfully loaded from file at {path}.");
+
 			return map;
+		}
+
+		/// <summary>
+		/// Saves map object to a json file at the specified path
+		/// </summary>
+		/// <param name="map"></param>
+		/// <param name="path"></param>
+		public static void SaveMap(Map map, string path) {
+			MapJSON json = new MapJSON(map._name, map._tilemapPath, map._size);
+			json.Data = map._data;
+
+			string text = JsonConvert.SerializeObject(json);
+			
+			if (!File.Exists(path))
+				Console.WriteLine($"[MAP SAVER] File at {path} does not exist! Will create new file.");
+			else
+				Console.WriteLine("$[MAP SAVER] File at {path} does exist! Will overwrite.");
+
+			File.WriteAllText(path, text);
 		}
 
 		// FUNCTIONS
@@ -92,6 +133,12 @@ namespace Topdown {
 		public Vector2 Size;
 		public int[,] Data;
 
+		/// <summary>
+		/// Initializes a new mapJSON object
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="path">Specifies the path to the tilemap</param>
+		/// <param name="size"></param>
 		public MapJSON(string name, string path, Vector2 size) {
 			Name = name;
 			TilemapPath = path;
