890e6d561bfeec33f3dcfa6d96e110e74ca8ec9a
NOTECOLT <6955795+NOTECOLT@users.noreply.github.com>
2024-09-02 11:33:49 +0800

    feat: basic battle textbox

--- some-rpg/Assets/Scripts/Battle/BattleManager.cs
+++ some-rpg/Assets/Scripts/Battle/BattleManager.cs
@@ -1,20 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour {
-    public TurnHolder CurrentTurn { get; private set; } = TurnHolder.NULL;
+    public BattleState CurrentState { get; private set; } = BattleState.PLAYER_SELECT;
+
+    [SerializeField] private TMP_Text _mainTextbox;
     void Start() {
         // Decide who is the first turnholder
     }
 
     void Update() {
-        
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            UpdateState();
+            _mainTextbox.text = CurrentState.ToString();
+        }
     }
 
     // Called by other entities to switch the current turn
-    public void SwitchTurn() {
-        CurrentTurn = (CurrentTurn == TurnHolder.PLAYER) ? TurnHolder.ENEMY : TurnHolder.PLAYER;
+    public void UpdateState() {
+        int states = Enum.GetNames(typeof(BattleState)).Length;
+        CurrentState = (BattleState)((int)(CurrentState + 1) % states);
     }
 
 }
---
+++ some-rpg/Assets/Scripts/Battle/BattleState.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+
+public enum BattleState {
+    PLAYER_SELECT,
+    ENEMY_SELECT,
+    ACTION_SEQUENCE // Performs all chosen actions in a turn
+}
