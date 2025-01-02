ed7b88db7447f6b7a87ad7c2e780c4f14bac41bd
notecolt <d27thColt@gmail.com>
2025-01-02 22:48:46 +0800

    feat: smoothened camera movement

--- some-rpg/Assets/Scripts/CameraFollow.cs
+++ some-rpg/Assets/Scripts/CameraFollow.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
     [SerializeField] private Transform focus;
+    [SerializeField] private float speed = 2;
 
     // Update is called once per frame
     void Update() {
         if (focus == null) return;
 
-        transform.position = new Vector3(focus.position.x, focus.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(focus.position.x, focus.position.y, transform.position.z), speed * Time.deltaTime);
+        // transform.position = new Vector3(focus.position.x, focus.position.y, transform.position.z);
     }
 }
